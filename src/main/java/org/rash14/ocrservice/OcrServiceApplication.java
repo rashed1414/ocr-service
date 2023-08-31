d29583aed256d2db53e7336cd1547a5224b9bffa
rashed <rashedrashed609@gmail.com>
2023-08-31 14:25:09 +0300

    init commit

---
+++ src/main/java/org/rash14/ocrservice/OcrServiceApplication.java
@@ -0,0 +1,13 @@
+package org.rash14.ocrservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OcrServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OcrServiceApplication.class, args);
+    }
+
+}
